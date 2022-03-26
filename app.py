88a84ffcdc5cc49fd4871c2a10797e00de667802
Kayden Ahn <kayden_ahn@intuit.com>
2022-03-26 16:11:37 -0700

    used US census data 2017

--- app.py
+++ app.py
@@ -9,19 +9,15 @@ import plotly.graph_objs as go
 
 from util import util as u
 
+# TODO: Below is good reference
+# Refer https://towardsdatascience.com/mapping-us-census-data-with-python-607df3de4b9c
+# https://plotly.com/python/mapbox-county-choropleth/
+# https://plotly.com/python/county-choropleth/ -> zill(5) as well
+
 # Global variables
 tabtitle = 'Virginia Counties'
-sourceurl = 'https://www.kaggle.com/muonneutrino/us-census-demographic-data'
+sourceurl = 'https://www.kaggle.com/muonneutrino/us-census-demographic-data?select=acs2017_census_tract_data.csv'
 githublink = 'https://github.com/cahn1/305-virginia-census-data/tree/update1'
-# options = [
-#     'TotalPop', 'Men', 'Women', 'Hispanic', 'White', 'Black', 'Native',
-#     'Asian', 'Pacific', 'VotingAgeCitizen', 'Income', 'IncomeErr',
-#     'IncomePerCap', 'IncomePerCapErr', 'Poverty', 'ChildPoverty',
-#     'Professional', 'Service', 'Office', 'Construction', 'Production',
-#     'Drive', 'Carpool', 'Transit', 'Walk', 'OtherTransp', 'WorkAtHome',
-#     'MeanCommute', 'Employed', 'PrivateWork', 'PublicWork', 'SelfEmployed',
-#     'FamilyWork', 'Unemployment', 'RUCC_2013']
-
 url = 'https://raw.githubusercontent.com/plotly/datasets/master/' \
       'geojson-counties-fips.json'
 
@@ -29,15 +25,11 @@ with urlopen(url) as response:
     counties = json.load(response)
 
 # https://www.geeksforgeeks.org/dataframe-read_pickle-method-in-pandas/
-df = pd.read_pickle('resources/va-stats.pkl')
-
-print(f'cahn0={df}')
-s1 = df['FIPS'].sample(5)
-print(f'df["FIPS"].sample(5)={s1}')
-print(f'df.columns={df.columns}')
-
+# add 'FIPS' column to df:
+# http://students.washington.edu/ayandm/tutfiles/FIPSConversion.pdf
+df = pd.read_csv('assets/census/acs2017_census_tract_data.csv')
+df['FIPS'] = df['County'].apply(u.pick_fip)
 options = u.pick_options(df)
-print(f'cahn4={options}')
 
 # app server config
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -47,7 +39,7 @@ app.title = tabtitle
 
 # app component layout
 app.layout = html.Div(children=[
-    html.H1('Virginia Census Data 2017'),
+    html.H1('US Census Demographic Data 2017'),
     html.Div(children=[
         # left side
         html.Div([
@@ -55,13 +47,13 @@ app.layout = html.Div(children=[
             dcc.Dropdown(
                 id='stats-drop',
                 options=[{'label': i, 'value': i} for i in options],
-                value='MeanCommute'
+                value='Carpool'
             ),
         ],
         className='three columns'),
         # right side
         html.Div([
-            dcc.Graph(id='va-map')],
+            dcc.Graph(id='us-map')],
             className='nine columns'
         ),
     ],
@@ -72,31 +64,43 @@ app.layout = html.Div(children=[
     html.A("Data Source", href=sourceurl),]
 )
 
+# colorscale = [
+#     'rgb(68.0, 1.0, 84.0)',
+#     'rgb(66.0, 64.0, 134.0)',
+#     'rgb(38.0, 130.0, 142.0)',
+#     'rgb(63.0, 188.0, 115.0)',
+#     'rgb(216.0, 226.0, 25.0)'
+# ]
+
 # callback
 @app.callback(
-    Output('va-map', 'figure'),
+    Output('us-map', 'figure'),
     [Input('stats-drop', 'value')])
 def display_results(option):
-    valmin = df[option].min()
-    valmax = df[option].max()
+    min_value = df[option].min()
+    max_value = df[option].max()
     fig = go.Figure(
         go.Choroplethmapbox(
             geojson=counties,
             locations=df['FIPS'],
             z=df[option],
-            colorscale='oxy',
+            colorscale='teal',
             text=df['County'],
-            zmin=valmin,
-            zmax=valmax,
+            zmin=min_value,
+            zmax=max_value,
+            showlegend=True,
             marker_line_width=0))
     fig.update_layout(
         mapbox_style="carto-positron",
-        mapbox_zoom=5.8,
-        mapbox_center = {"lat": 38.0293, "lon": -79.4428})
+        mapbox_zoom=3.0,
+        # https://docs.mapbox.com/playground/geocoding/?search_text=-98
+        # .62651607972974,40.33851776440284&types=place%2Cpostcode%2Caddress&limit=1
+        mapbox_center={"lat": 37.0902, "lon": -95.7129})
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
     # https://community.plot.ly/t/what-colorscales-are-available-in-plotly-and
     # -which-are-the-default/2079
+
     return fig
 
 
--- util/util.py
+++ util/util.py
@@ -1,3 +1,10 @@
+import json
+import pandas as pd
+
+
+fips_master = pd.read_csv('resources/county_fips_master.csv')[
+    ['fips', 'county_name']]
+county_fips = json.loads(fips_master.to_json(orient='records'))
 
 
 def pick_options(df):
@@ -6,3 +13,10 @@ def pick_options(df):
         if df[col].dtype == float or df[col].dtype == int:
             valid_options.append(col)
     return valid_options
+
+
+def pick_fip(county):
+    for record in county_fips:
+        if record.get('county_name', '').lower() == county.lower():
+            return str(record.get('fips', '')).zfill(5)
+    return ''
